13cb519774bbe3cc564ececc8ee46020a2334f11
unknown <aminchev1988@gmail.com>
2025-01-15 19:43:24 +0200

    Commiting the current processing of the task

---
+++ views/catTemplate.html.js
@@ -0,0 +1,12 @@
+export default (cat) => `
+        <li>
+            <img src="${cat.imageUrl}" alt="${cat.id}">
+            <h3>${cat.name}</h3>
+            <p><span>Breed: </span>${cat.breed}</p>
+            <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="/cats/edit-cat">Change Info</a></li>
+                <li class="btn delete"><a href="/cats/cat-shelter">New Home</a></li>
+            </ul>
+        </li>
+`
